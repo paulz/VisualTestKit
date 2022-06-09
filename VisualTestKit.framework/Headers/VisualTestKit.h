1aea5eb87f7a7ccc9d52361f98a89f444e858573
Paul Zabelin <paulz@users.noreply.github.com>
2022-06-09 01:18:29 -0700

    add class dump headers

--- VisualTestKit.framework/Headers/VisualTestKit.h
+++ VisualTestKit.framework/Headers/VisualTestKit.h
@@ -1,3 +1,5 @@
 #import <VisualTestKit/VTKAssertID.h>
 #import <VisualTestKit/VTKAssert.h>
 #import <VisualTestKit/XCTestCase+VTKAssert.h>
+#import <VisualTestKit/_VTKColor.h>
+#import <VisualTestKit/UIView-VTKSnapshot.h>
