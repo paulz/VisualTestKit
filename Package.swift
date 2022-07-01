ad8d87708097fb80071a427f52c5224ddf533be2
Paul Zabelin <paulz@users.noreply.github.com>
2022-07-01 15:57:11 -0700

    add swift package

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VisualTestKit",
+    products: [
+        .library(
+            name: "VisualTestKit",
+            targets: ["VisualTestKit"]),
+    ],
+    targets: [
+        .binaryTarget(name: "VisualTestKit", path: "VisualTestKit.xcframework")
+    ]
+)
