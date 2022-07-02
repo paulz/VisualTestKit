d93aaf96aaaf95ce0179891a9b34a7e719cd15a9
Paul Zabelin <paulz@users.noreply.github.com>
2022-07-01 23:51:12 -0700

    add plaform value

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "VisualTestKit",
+    platforms: [
+        .iOS(.v12),
+    ],
     products: [
         .library(
             name: "VisualTestKit",
